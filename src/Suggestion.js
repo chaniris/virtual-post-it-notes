400129fe0951e31561fed94e61fa8486f184975b
Iris <iris_chan_77@hotmail.com>
2021-11-09 20:35:38 -0500

    Add loading gif to api fetch

--- src/Suggestion.js
+++ src/Suggestion.js
@@ -1,9 +1,12 @@
+import gif from "./assets/giphy.gif"
+
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const Suggestion = () => {
 
     const [suggestions, setSuggestions] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios({
@@ -12,17 +15,29 @@ const Suggestion = () => {
             dataResponse: 'json',
         }).then((res) => {
             setSuggestions(res.data);
+            setLoading(false);
         });
     }, []);
 
-    return (
-        <div className='suggestionContainer'>
-            <h3>Idea of the Day</h3>
-            <p key={suggestions.key}>
-                {suggestions.activity}
-            </p>
-        </div>
-    );
+    if (loading) {
+        return (
+            <div className='suggestionContainer'>
+                <h3>Idea of the Day</h3>
+                <div class="loading">
+                    <img src={gif} alt="Loading" />
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className='suggestionContainer'>
+                <h3>Idea of the Day</h3>
+                <p key={suggestions.key}>
+                    {suggestions.activity}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default Suggestion;
